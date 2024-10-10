c39cc8c2d0d3233216112c9391cff6c59c67c081
Thanutham-t <thanutham.tn@gmail.com>
2024-10-10 22:01:57 +0700

    Create Function to assign Labels

---
+++ server/infer_labels.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def infer_data_labels(X_labels, cluster_labels):
+    """
+    Determines label for each array, depending on the cluster it has been assigned to.
+    returns: predicted labels for each array
+    """
+
+    # empty array of len(X)
+    predicted_labels = np.zeros(len(X_labels)).astype(np.uint8)
+
+    for i, cluster in enumerate(X_labels):
+        for key, value in cluster_labels.items():
+            if cluster in value:
+                predicted_labels[i] = key
+
+    return predicted_labels
